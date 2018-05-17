66f6f8c023445d064311eae7b0b66d7bdd4b9a02
kefche <kefche2003@hotmail.com>
2018-05-17 17:40:27 +0300

    Default
    No updates.

---
+++ app/src/main/java/e/kefch_000/e_school/MainActivity.java
@@ -0,0 +1,13 @@
+package e.kefch_000.e_school;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
